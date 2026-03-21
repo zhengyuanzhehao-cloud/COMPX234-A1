e6217d0dee318fd327655119a1e83d301e07c63f
zhengyuanzhehao-cloud <zhengyuanzhehao@163.com>
2026-03-21 13:44:22 +0800

    llll

--- Assignment1Task.py
+++ Assignment1Task.py
@@ -18,7 +18,11 @@ class Assignment1:
         self.sim_active = True
         self.print_list = printList()  # Create an empty list of print requests创建一个空的打印请求队列
         self.mThreads = []             # list for machine threads机器线程列表
-        self.pThreads = []             # list for printer threads打印机线程列表
+        self.pThreads = []  
+                   # list for printer threads打印机线程列表
+        self.queue_lock = threading.Lock()            
+        self.empty_slots = threading.Semaphore(self.QUEUE_CAPACITY)  
+        self.full_slots = threading.Semaphore(0)
 
     def startSimulation(self):
         # Create Machine and Printer threads
@@ -54,6 +58,10 @@ class Assignment1:
 
         # Wait until all printer threads finish by joining them
         # Write code here
+        for _ in range(self.NUM_MACHINES):
+            self.empty_slots.release()
+        for _ in range(self.NUM_PRINTERS):
+            self.full_slots.release()
         for t in self.pThreads:
             t.join()
         for t in self.mThreads:
@@ -69,10 +77,17 @@ class Assignment1:
         def run(self):
             while self.outer.sim_active:
                 # Simulate printer taking some time to print the document
-                self.printerSleep()
+                
                 # Grab the request at the head of the queue and print it
                 # Write code here
-                self
+                self.outer.full_slots.acquire()  # Wait until there is at least one print request in the queue
+                if not self.outer.sim_active:
+                    break
+
+                with self.outer.queue_lock:  # Lock the queue to safely access it
+                    self.printDox(self.printerID)  # Print the document at the head of the queue
+                self.outer.empty_slots.release()  # Signal that there is now one more empty slot in the queue
+                self.printerSleep()  # Simulate the printer taking some time to print the document
 
         def printerSleep(self):
             sleepSeconds = random.randint(1, self.outer.MAX_PRINTER_SLEEP)
@@ -96,6 +111,17 @@ class Assignment1:
                 self.machineSleep()
                 # Machine wakes up and sends a print request
                 # Write code here
+                if not self.outer.sim_active:
+                    break
+                self.outer.empty_slots.acquire()  # Wait until there is at least one empty slot in the queue
+                if not self.outer.sim_active:
+                    self.outer.empty_slots.release()  # Release the slot if the simulation is no longer active
+                    break
+
+                with self.outer.queue_lock:  # Lock the queue to safely access it
+                    self.printRequest(self.machineID)  # Send a print request
+
+                self.outer.full_slots.release()  # Signal that there is now one more full slot in the queue
 
         def machineSleep(self):
             sleepSeconds = random.randint(1, self.outer.MAX_MACHINE_SLEEP)
